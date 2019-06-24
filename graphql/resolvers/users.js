566ad02e364f37126d147a695d32f5769109f504
Jarkko <jarkkohei@luukku.com>
2019-06-24 22:24:48 +0300

    Add checking if the username has already been taken and throw an apollo-error if it has

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { UserInputError } = require('apollo-server');
 
 const { JWT_SECRET_KEY } = require('../../config');
 const User = require('../../models/User');
@@ -16,6 +17,17 @@ module.exports = {
             } 
         }, 
             context, info) {
+            
+            const user = await User.findOne({ username });
+
+            if(user) {
+                throw new UserInputError('Username is taken', {
+                    errors: {
+                        username: 'This username is already taken'
+                    }
+                });
+            }
+
             password = await bcrypt.hash(password, 12);
 
             const newUser = new User({
