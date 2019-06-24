0988469353943930f2a8732520996c53b917e19d
Jarkko <jarkkohei@luukku.com>
2019-06-24 22:45:41 +0300

    Add validation to the registering

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { UserInputError } = require('apollo-server');
 
+const { validateRegisterInput } = require('../../util/validators');
 const { JWT_SECRET_KEY } = require('../../config');
 const User = require('../../models/User');
 
@@ -12,6 +13,11 @@ module.exports = {
             { registerInput : { username, email, password, confirmPassword } 
         }, 
             context, info) {
+
+            const { valid, errors } = validateRegisterInput(username, email, password, confirmPassword);
+            if(!valid) {
+                throw new UserInputError('Errors', { errors });
+            }
             
             const user = await User.findOne({ username });
 
---
+++ util/validators.js
@@ -0,0 +1,35 @@
+module.exports.validateRegisterInput = (
+    username,
+    email,
+    password,
+    confirmPassword
+) => {
+    const errors = {};
+
+    if(username.trim() === '') {
+        errors.username = 'Username must not be empty';
+    }
+
+    if (email.trim() === '') {
+        errors.email = 'Email must not be empty';
+    } else {
+        const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
+
+        if(!email.match(regEx)) {
+            errors.email = 'Email must be a valid email address';
+        }
+    }
+
+    if(password === '') {
+        errors.password = 'Password must not be empty';
+    } else if(password.length < 6) {
+        errors.password = 'Password must be at least six (6) characters long';
+    } else if(password !== confirmPassword) {
+        errors.confirmPassword = 'Passwords do not match.';
+    }
+
+    return {
+        errors,
+        valid: Object.keys(errors).length < 1
+    }
+}
