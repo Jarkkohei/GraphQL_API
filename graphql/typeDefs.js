b1a0115d63eb2801d91d9db764bf8f8a700b3ccc
Jarkko <jarkkohei@luukku.com>
2019-06-26 05:04:31 +0300

    Add functionality for liking/unliking posts

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -50,5 +50,6 @@ module.exports = gql`
         deletePost(postId: ID!): String!
         createComment(postId: ID!, body: String!): Post!
         deleteComment(postId: ID!, commentId: ID!): Post!
+        likePost(postId: ID!): Post!
     }
 `;
