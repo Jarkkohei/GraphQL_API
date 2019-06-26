bfa5ed6bafcfe8c8bd9953ada2c99356fdb1d0d3
Jarkko <jarkkohei@luukku.com>
2019-06-26 05:31:00 +0300

    Add calculated values likeCount and commentCount for posts

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -3,6 +3,14 @@ const usersResolvers = require('./users');
 const commentsResolvers = require('./comments');
 
 module.exports = {
+    Post: {
+        likeCount(parent) {
+            //console.log(parent);
+            return parent.likes.length;
+        },
+        // Arrow function example
+        commentCount: (parent) => parent.comments.length
+    },
     Query: {
         ...postsResolvers.Query
     },
