830826a794037f6829b0390878c41f1c0081371e
Jarkko <jarkkohei@luukku.com>
2019-06-27 16:47:36 +0300

    Add functionality to the Register-component

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,14 +1,32 @@
 import React, { useState } from 'react'
 import { Form, Button } from 'semantic-ui-react';
+import { useMutation } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 
 function Register() {
 
-    const [values, setValus] = useState({
+    const [values, setValues] = useState({
         username: '',
         email: '',
         password: '',
         confirmPassword: '',
     });
+
+    const onChange = (event) => {
+        setValues({...values, [event.target.name]: event.target.value});
+    }
+
+    const [addUser, { loading }] = useMutation(REGISTER_USER, {
+        update(proxy, result) {
+            console.log(result);
+        },
+        variables: values
+    });
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        addUser();
+    }
 
     return (
         <div>
@@ -49,5 +67,29 @@ function Register() {
         </div>
     )
 }
+
+const REGISTER_USER = gql`
+    mutation register(
+        $username: String!
+        $email: String!
+        $password: String!
+        $confirmPassword: String!
+    ) {
+        register(
+            registerInput: {
+                username: $username
+                email: $email
+                password: $password
+                confirmPassword: $confirmPassword
+            }
+        ) {
+            id
+            email
+            username
+            created_at
+            token
+        }
+    }
+`;
 
 export default Register;
