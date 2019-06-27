ea3bdf09aa988c675e65ef5b99e6d2b0534b64e3
Jarkko <jarkkohei@luukku.com>
2019-06-27 12:41:53 +0300

    Reactor class-based MenuBar-component to functional component and also fix the links in it

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,41 +1,32 @@
-import React, { Component } from 'react'
-import { Menu, Segment } from 'semantic-ui-react'
+import React, { useState } from 'react'
+import { Menu } from 'semantic-ui-react'
 
-export default class MenuExampleSecondaryPointing extends Component {
-    state = { activeItem: 'home' }
+function MenuBar() {
+    const [activeItem, setActiveItem] = useState('');
 
-    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+    const handleItemClick = (e, { name }) => setActiveItem(name);
 
-    render() {
-        const { activeItem } = this.state
-
-        return (
-            <div>
-                <Menu pointing secondary>
-                    <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
-                    <Menu.Item
-                        name='messages'
-                        active={activeItem === 'messages'}
-                        onClick={this.handleItemClick}
-                    />
-                    <Menu.Item
-                        name='friends'
-                        active={activeItem === 'friends'}
-                        onClick={this.handleItemClick}
-                    />
-                    <Menu.Menu position='right'>
-                        <Menu.Item
-                            name='logout'
-                            active={activeItem === 'logout'}
-                            onClick={this.handleItemClick}
-                        />
-                    </Menu.Menu>
-                </Menu>
-
-                <Segment>
-                    <img src='/images/wireframe/media-paragraph.png' />
-                </Segment>
-            </div>
-        )
-    }
+    return (
+        <Menu pointing secondary>
+            <Menu.Item
+                name='home'
+                active={activeItem === 'home'}
+                onClick={handleItemClick}
+            />
+            <Menu.Menu position='right'>
+                <Menu.Item
+                    name='login'
+                    active={activeItem === 'login'}
+                    onClick={handleItemClick}
+                />
+                <Menu.Item
+                    name='register'
+                    active={activeItem === 'register'}
+                    onClick={handleItemClick}
+                />
+            </Menu.Menu>
+        </Menu>
+    )
 }
+
+export default MenuBar;
