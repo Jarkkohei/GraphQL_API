202c961b9908b18016684fd02781dd8f71f4b55b
Jarkko <jarkkohei@luukku.com>
2019-06-27 13:48:59 +0300

    Make a getPosts query and deconstruct the fetched data as posts

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 
 function Home() {
+    const { loading, data: { getPosts: posts } } = useQuery(FETCH_POSTS_QUERY);
+
+    if(posts) {
+        console.log(posts);
+    }
+
     return (
         <div>
             <h1>Home Page</h1>
         </div>
     )
 }
+
+const FETCH_POSTS_QUERY = gql`
+    {
+        getPosts{
+            id 
+            body 
+            created_at 
+            username 
+            likeCount
+            likes{
+                username
+            }
+            commentCount
+            comments{
+                id 
+                username 
+                created_at 
+                body
+            }
+        }
+    }
+`;
 
 export default Home;
