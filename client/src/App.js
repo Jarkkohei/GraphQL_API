aeea3b8e431d08444718bee41e17095d1d722c18
Jarkko <jarkkohei@luukku.com>
2019-06-27 12:28:20 +0300

    Add Page-components and routing for them in App-component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div><h1>Social Media App</h1></div>
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+    </Router>
   );
 }
 
