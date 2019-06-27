87af48de356513078509c6de8ee9fe5bdb5c9863
Jarkko <jarkkohei@luukku.com>
2019-06-27 17:18:02 +0300

    Show errors and enable Register- form inputs error-styling

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -45,6 +45,7 @@ function Register(props) {
                     type="text"
                     value={values.username}
                     onChange={onChange}
+                    error={errors.username ? true : false}
                 />
                 <Form.Input
                     label="Email"
@@ -53,6 +54,7 @@ function Register(props) {
                     type="email"
                     value={values.email}
                     onChange={onChange}
+                    error={errors.email ? true : false}
                 />
                 <Form.Input
                     label="Password"
@@ -61,6 +63,7 @@ function Register(props) {
                     type="password"
                     value={values.password}
                     onChange={onChange}
+                    error={errors.password ? true : false}
                 />
                 <Form.Input
                     label="Confirm Password"
@@ -69,13 +72,24 @@ function Register(props) {
                     type="password"
                     value={values.confirmPassword}
                     onChange={onChange}
+                    error={errors.confirmPassword ? true : false}
                 />
                 <Button type="submit" primary>
                     Register
                 </Button>
             </Form>
+
+            {Object.keys(errors).length > 0 && (
+                <div className="ui error message">
+                    <ul className="list">
+                        {Object.values(errors).map(value => (
+                            <li key={value}>{value}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
         </div>
-    )
+    );
 }
 
 const REGISTER_USER = gql`
