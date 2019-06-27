a6a405394b10517c0113a8470ada6297bd1823d3
Jarkko <jarkkohei@luukku.com>
2019-06-27 16:34:28 +0300

    Add Form to Login page

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Form, Button } from 'semantic-ui-react';
 
 function Register() {
+
+    const [values, setValus] = useState({
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+    });
+
     return (
         <div>
-            <h1>Register Page</h1>
+            <Form onSubmit={onSubmit} noValidate>
+                <h1>Register</h1>
+                <Form.Input
+                    label="Username"
+                    placeholder="Username..."
+                    name="username"
+                    value={values.username}
+                    onChange={onChange}
+                />
+                <Form.Input
+                    label="Email"
+                    placeholder="Email..."
+                    name="email"
+                    value={values.email}
+                    onChange={onChange}
+                />
+                <Form.Input
+                    label="Password"
+                    placeholder="Password..."
+                    name="password"
+                    value={values.password}
+                    onChange={onChange}
+                />
+                <Form.Input
+                    label="Confirm Password"
+                    placeholder="Confirm Password..."
+                    name="confirmPassword"
+                    value={values.confirmPassword}
+                    onChange={onChange}
+                />
+                <Button type="submit" primary>
+                    Register
+                </Button>
+            </Form>
         </div>
     )
 }
