9cababa8753a9504f20c7fb21cecc7acc4dbf1a1
Jarkko <jarkkohei@luukku.com>
2019-06-27 14:19:05 +0300

    Make the PostCard Meta (created_at date) as a link to the post and add a title property for it

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card, Icon, Label, Image } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 import moment from 'moment';
 
 function PostCard({ post: { id, body, created_at, username, likeCount, commentCount, likes }}) {
@@ -9,7 +10,7 @@ function PostCard({ post: { id, body, created_at, username, likeCount, commentCo
             <Card.Content>
                 <Image floated='right' size='mini' src='https://react.semantic-ui.com/images/avatar/large/molly.png' />
                 <Card.Header>{username}</Card.Header>
-                <Card.Meta>{moment(created_at).fromNow()}</Card.Meta>
+                <Card.Meta as={Link} to={`/posts/${id}`} title="Go to the post">{moment(created_at).fromNow()}</Card.Meta>
                 <Card.Description>{body}</Card.Description>
             </Card.Content>
             <Card.Content extra>
