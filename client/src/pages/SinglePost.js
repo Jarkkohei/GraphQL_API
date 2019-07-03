39ba93ca63ccc595ca6ff4b0da47f7700c6c80a2
Jarkko <jarkkohei@luukku.com>
2019-07-03 17:46:52 +0300

    Add buttons for liking and commenting the post to the SinglePost-page

--- client/src/pages/SinglePost.js
+++ client/src/pages/SinglePost.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import gql from 'graphql-tag';
 import { useQuery } from '@apollo/react-hooks';
-import { Grid, Image, Card } from 'semantic-ui-react';
+import { Grid, Image, Card, Button, Icon, Label } from 'semantic-ui-react';
 import moment from 'moment';
+import LikeButton from '../components/LikeButton';
+
+import { AuthContext } from '../context/auth';
 
 function SinglePost(props) {
 
     const postId = props.match.params.postId;
+
+    const { user } = useContext(AuthContext);
 
     const { data: { getPost } } = useQuery(FETCH_POST_QUERY, {
         variables: {
@@ -37,6 +42,17 @@ function SinglePost(props) {
                                <Card.Description>{ body }</Card.Description>
                            </Card.Content>
                            <hr/>
+                           <Card.Content>
+                               <LikeButton user={user} post={{ id, likeCount, likes }} />
+                               <Button as="div" labelPosition="right" onClick={() => console.log('Comment on post')}>
+                                   <Button basic color="teal">
+                                       <Icon name="comments" />
+                                   </Button>
+                                   <Label basic color="teal" pointing="left">
+                                       {commentCount}
+                                   </Label>
+                               </Button>
+                           </Card.Content>
                        </Card>
                     </Grid.Column>
                 </Grid.Row>
