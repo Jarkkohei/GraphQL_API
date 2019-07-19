b60c66f99fdf627c19f1931f9c81a74071eada18
Jarkko <jarkkohei@luukku.com>
2019-07-19 13:25:17 +0300

    Prettify the code

--- client/src/pages/SinglePost.js
+++ client/src/pages/SinglePost.js
@@ -27,7 +27,7 @@ function SinglePost(props) {
 
     let postMarkup;
 
-    if(!getPost) {
+    if (!getPost) {
         // While waiting for the post...
         postMarkup = <p>Loading post...</p>
     } else {
@@ -41,28 +41,28 @@ function SinglePost(props) {
                         <Image src='https://react.semantic-ui.com/images/avatar/large/molly.png' size="small" float="right" />
                     </Grid.Column>
                     <Grid.Column width={10}>
-                       <Card fluid>
-                           <Card.Content>
-                               <Card.Header>{ username }</Card.Header>
-                               <Card.Meta>{ moment(created_at).fromNow() }</Card.Meta>
-                               <Card.Description>{ body }</Card.Description>
-                           </Card.Content>
-                           <hr/>
-                           <Card.Content>
-                               <LikeButton user={user} post={{ id, likeCount, likes }} />
-                               <Button as="div" labelPosition="right" onClick={() => console.log('Comment on post')}>
-                                   <Button basic color="teal">
-                                       <Icon name="comments" />
-                                   </Button>
-                                   <Label basic color="teal" pointing="left">
-                                       {commentCount}
-                                   </Label>
-                               </Button>
-                               {user && user.username === username && (
-                                   <DeleteButton postId={id} callback={deletePostCallback} />
-                               )}
-                           </Card.Content>
-                       </Card>
+                        <Card fluid>
+                            <Card.Content>
+                                <Card.Header>{username}</Card.Header>
+                                <Card.Meta>{moment(created_at).fromNow()}</Card.Meta>
+                                <Card.Description>{body}</Card.Description>
+                            </Card.Content>
+                            <hr />
+                            <Card.Content>
+                                <LikeButton user={user} post={{ id, likeCount, likes }} />
+                                <Button as="div" labelPosition="right" onClick={() => console.log('Comment on post')}>
+                                    <Button basic color="teal">
+                                        <Icon name="comments" />
+                                    </Button>
+                                    <Label basic color="teal" pointing="left">
+                                        {commentCount}
+                                    </Label>
+                                </Button>
+                                {user && user.username === username && (
+                                    <DeleteButton postId={id} callback={deletePostCallback} />
+                                )}
+                            </Card.Content>
+                        </Card>
                     </Grid.Column>
                 </Grid.Row>
             </Grid>
